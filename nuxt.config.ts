1cb87ccb5287d845ce7445f0374c42cffc28c630
Camilo Arias <camilo.arias@uptc.edu.co>
2026-06-22 18:43:53 -0500

    fix: set logo as browser favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,9 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Deck Royale - Poker Multijugador',
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/images/logo.png' },
+      ],
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
